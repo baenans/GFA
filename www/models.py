ca9db237a4518d890866e730a130885dbf564d39
Francisco Baena <yo@baenafrancisco.com>
2013-10-31 17:15:04 +0100

    Modelado completo

--- www/models.py
+++ www/models.py
@@ -1,7 +1,14 @@
-
+# -*- coding: utf-8 -*-
 from django.db import models
-
 # Create your models here.
 class Planta(models.Model):
-	nombreComun = models.CharField(max_lenght=150)
-	nombreCientifico = models.CharField(max_lenght=150)
+	nombreComun = models.CharField(max_length=150)
+	nombreCientifico = models.CharField(max_length=150)
+	foto= models.ImageField(upload_to="uploads/")
+	detalleHoja= models.ImageField(upload_to="uploads/")
+	distribucion = models.CharField(max_length=100)
+	medida = models.DecimalField(max_digits=6, decimal_places=2)
+	tipoDeSuelo = models.CharField(max_length=100)
+	altura = models.DecimalField(max_digits=7, decimal_places=2)
+	def __unicode__(self):
+		return  u"%s (%s)" % (self.nombreCientifico, self.nombreComun)
---
+++ www/settings.py
@@ -0,0 +1,96 @@
+"""
+Django settings for plantas project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/dev/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'i3q6e8&8d5&77_-c7f12mv-2h97jz3h-j4v%^ltg)fpk@#dlog'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'www'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'www.urls'
+
+WSGI_APPLICATION = 'www.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/dev/topics/i18n/
+
+LANGUAGE_CODE = 'es-es'
+
+TIME_ZONE = 'Europe/Madrid'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/dev/howto/static-files/
+
+STATIC_URL = '/static/'
+
+ROOT = os.path.join(os.path.dirname(__file__), "..")
+
+
+MEDIA_ROOT = os.path.join(ROOT, "media/")
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
