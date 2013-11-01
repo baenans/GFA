ab8a65fdf1451ff8fcfb3ce3a84490334099a06d
Francisco Baena <yo@baenafrancisco.com>
2013-11-01 22:06:23 +0100

    Añadido index

--- www/urls.py
+++ www/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +8,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'www.views.index', name='index'),
+    url(r'^login/$', 'www.views.login', name='login'),
+
 )
--- www/views.py
+++ www/views.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='/login/')
+def index(request):
+	context = {}
+	return render(request, 'index.htm', context)
+def login(request):
+	context = {}
+	return render(request, 'index.htm', context)
